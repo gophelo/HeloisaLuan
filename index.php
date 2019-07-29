dfa1239b2a7540efc08d6f05b34e2d83f06dfbb1
Escola <Escola@DESKTOP-KJI13GO>
2019-07-29 16:16:04 -0300

    commit do luan lindo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title> Luan Lindo </title>
     </head>
     <body>
         <?php
